07cd4fa81c6796c1de20fbaca0b062627e67410a
Stefano Della Valle <sdellava@gmail.com>
2026-06-08 16:19:55 +0200

    Use ObjectID Android launcher icon

--- objectid/src-tauri/gen-static/apply.sh
+++ objectid/src-tauri/gen-static/apply.sh
@@ -15,6 +15,7 @@ copy_file() {
 }
 
 copy_file ./android/app/proguard-rules.pro ../gen/android/app/proguard-rules.pro
+cp -R ./android/app/src/main/res/. ../gen/android/app/src/main/res/
 copy_file ./android/app/src/main/java/com/impierce/identity_wallet/MainActivity.kt ../gen/android/app/src/main/java/com/impierce/identity_wallet/MainActivity.kt
 copy_file ./android/app/src/main/AndroidManifest.xml ../gen/android/app/src/main/AndroidManifest.xml
 copy_file ./apple/ExportOptions.plist ../gen/apple/ExportOptions.plist
